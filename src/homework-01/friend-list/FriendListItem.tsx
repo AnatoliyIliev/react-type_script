3bf7d76de274f28cad30aa2eeff561ccc5399e58
AnatoliyIliev <vekok12@gmail.com>
2022-03-26 20:41:36 +0200

    fix: FriendListItem

--- src/homework-01/friend-list/FriendListItem.tsx
+++ src/homework-01/friend-list/FriendListItem.tsx
@@ -9,7 +9,10 @@ interface Props {
 function FriendListItem({ avatar, name, isOnline }: Props) {
   return (
     <>
-      <span className="status"></span>
+      <span
+        className="status"
+        style={{ background: isOnline ? 'green' : 'red' }}
+      ></span>
       <img className="avatar" src={avatar} alt={name} width="48" />
       <p className="name">{name}</p>
     </>

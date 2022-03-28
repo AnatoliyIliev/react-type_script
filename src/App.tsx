da2fd4a966f354f4e3cd2b529ca78864f7c17488
AnatoliyIliev <vekok12@gmail.com>
2022-03-28 16:03:35 +0300

    feat: Statistics

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import FriendList from './homework-01/friend-list/FriendList';
 import Profile from './homework-01/social-profile/Profile';
+import Statistics from './homework-01/statistics/Statistics';
 
 import friends from './homework-01/friend-list/friends.json';
 import user from './homework-01/social-profile/user.json';
+import data from './homework-01/statistics/data.json';
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 }

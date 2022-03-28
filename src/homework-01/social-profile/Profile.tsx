65f4f507e91b234ef0e74f9063a5a181bb82c884
AnatoliyIliev <vekok12@gmail.com>
2022-03-28 12:30:55 +0300

    feat: Profile styles

--- src/homework-01/social-profile/Profile.tsx
+++ src/homework-01/social-profile/Profile.tsx
@@ -17,21 +17,21 @@ function Profile({ username, tag, location, avatar, stats }: Props) {
     <div className="profile">
       <div className="description">
         <img src={avatar} alt={username} className="profile_avatar" />
-        <p className="name">{username}</p>
+        <p className="profile_name">{username}</p>
         <p className="tag">@{tag}</p>
         <p className="location">{location}</p>
       </div>
 
-      <ul className="stats">
-        <li>
+      <ul className="profile_stats">
+        <li className="profile_data">
           <span className="label">Followers</span>
           <span className="quantity">{stats.followers}</span>
         </li>
-        <li>
+        <li className="profile_data">
           <span className="label">Views</span>
           <span className="quantity">{stats.views}</span>
         </li>
-        <li>
+        <li className="profile_data">
           <span className="label">Likes</span>
           <span className="quantity">{stats.likes}</span>
         </li>

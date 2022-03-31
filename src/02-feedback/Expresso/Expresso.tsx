d7dd0a7f31971dd20d2aa00fd00e518cfd671b97
AnatoliyIliev <vekok12@gmail.com>
2022-03-31 22:25:26 +0300

    init: Expresso

--- src/02-feedback/Expresso/Expresso.tsx
+++ src/02-feedback/Expresso/Expresso.tsx
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
+var shortid = require('shortid');
 
-class Expresso extends React.Component {
+interface Props {}
+
+interface State {
+  good: number;
+  neutral: number;
+  bad: number;
+}
+
+class Expresso extends Component<Props, State> {
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+    good: 1,
+    neutral: 2,
+    bad: 3,
+  };
+
+  options = ['good', 'neutral', 'bad'];
+
+  handleChange = (event: any) => {
+    console.log(event);
   };
 
   render() {
-    return <div>Hello</div>;
+    // const { state } = this;
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+
+        {this.options.map(option => (
+          <button
+            type="button"
+            name={option}
+            key={shortid.generate()}
+            onClick={this.handleChange}
+          >
+            {option[0].toUpperCase() + option.slice(1)}
+          </button>
+        ))}
+
+        <h2>Statistics</h2>
+        {false ? (
+          <p>No feedback given</p>
+        ) : (
+          <ul>
+            {this.options.map(option => (
+              <li key={shortid.generate()}>{option}:</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    );
   }
 }
 

67fb05bb80f913992e106811aacf0b346e8e65c3
AnatoliyIliev <vekok12@gmail.com>
2022-04-01 21:41:19 +0300

    init: countTotalFeedback

--- src/02-feedback/Expresso/Expresso.tsx
+++ src/02-feedback/Expresso/Expresso.tsx
@@ -1,29 +1,43 @@
 import React, { Component } from 'react';
 var shortid = require('shortid');
 
-interface Props {}
-
-interface State {
+interface IState {
   good: number;
   neutral: number;
   bad: number;
 }
 
-class Expresso extends Component<Props, State> {
+class Expresso extends Component<{}, IState> {
   state = {
-    good: 1,
-    neutral: 2,
-    bad: 3,
+    good: 0,
+    neutral: 0,
+    bad: 0,
   };
 
-  options = ['good', 'neutral', 'bad'];
+  options = Object.keys(this.state);
 
-  handleChange = (event: any) => {
-    console.log(event);
+  handleChange = (option: string) => {
+    this.setState((prevState: any) => {
+      return {
+        ...prevState,
+        [option]: prevState[option] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    let percentage = Math.ceil((good / this.countTotalFeedback()) * 100);
+    return percentage;
   };
 
   render() {
-    // const { state } = this;
+    const { state } = this;
     return (
       <div>
         <h1>Please leave feedback</h1>
@@ -33,21 +47,29 @@ class Expresso extends Component<Props, State> {
             type="button"
             name={option}
             key={shortid.generate()}
-            onClick={this.handleChange}
+            onClick={() => this.handleChange(option)}
           >
             {option[0].toUpperCase() + option.slice(1)}
           </button>
         ))}
 
         <h2>Statistics</h2>
-        {false ? (
+        {this.countTotalFeedback() < 0 ? (
           <p>No feedback given</p>
         ) : (
-          <ul>
-            {this.options.map(option => (
-              <li key={shortid.generate()}>{option}:</li>
-            ))}
-          </ul>
+          <>
+            <ul>
+              {this.options.map(option => (
+                <li key={shortid.generate()}>
+                  {option[0].toUpperCase() + option.slice(1)}: {option}
+                </li>
+              ))}
+            </ul>
+            <p>Total: {this.countTotalFeedback()}</p>
+            <p>
+              Positive percentage: {this.countPositiveFeedbackPercentage()}%
+            </p>
+          </>
         )}
       </div>
     );

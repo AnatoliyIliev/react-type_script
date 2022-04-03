809a16b6b060773f47c943e52a7ad9129633119e
AnatoliyIliev <vekok12@gmail.com>
2022-04-03 12:28:20 +0300

    init: start Phonebook

---
+++ src/Phonebook.tsx
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+class Phonebook extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  render() {
+    return <div>Hello</div>;
+  }
+}
+
+export default Phonebook;

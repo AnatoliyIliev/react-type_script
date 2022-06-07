846af629cbea82263c44f2778f3a9e65e24e3416
AnatoliyIliev <vekok12@gmail.com>
2022-06-07 17:54:14 +0300

    refactor: del unnecessary

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import Components from './Components';
-// import Feedback from './Feedback';
 import Phonebook from './Phonebook';
 import 'modern-normalize/modern-normalize.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Components /> */}
-    {/* <Feedback /> */}
     <Phonebook />
   </React.StrictMode>,
   document.getElementById('root'),

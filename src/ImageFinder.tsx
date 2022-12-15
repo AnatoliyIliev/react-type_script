45bea091a762b089b9c5ffac8b8de3d230b9672f
AnatoliyIliev <vekok12@gmail.com>
2022-12-15 19:26:43 +0200

    init: start image finder

---
+++ src/ImageFinder.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ImageFinder extends Component {
+  render() {
+    return <div>Hello</div>;
+  }
+}
+
+export default ImageFinder;
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import Components from './Components';
 // import Feedback from './Feedback';
-import Phonebook from './Phonebook';
+// import Phonebook from './Phonebook';
+import ImageFinder from './ImageFinder';
 import 'modern-normalize/modern-normalize.css';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <Components /> */}
     {/* <Feedback /> */}
-    <Phonebook />
+    {/* <Phonebook /> */}
+    <ImageFinder />
   </React.StrictMode>,
   document.getElementById('root'),
 );

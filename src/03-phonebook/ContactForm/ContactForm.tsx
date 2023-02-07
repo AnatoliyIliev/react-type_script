4bcc9342556962434266ce391855cd105bc3f409
AnatoliyIliev <vekok12@gmail.com>
2023-02-07 21:18:31 +0200

    init: start 03-phonebook

---
+++ src/03-phonebook/ContactForm/ContactForm.tsx
@@ -0,0 +1,82 @@
+import { Component } from 'react';
+import '../Phonebook03.css';
+
+interface IProps {
+  onSubmitForm: (state: { name: string; number: string }) => void;
+}
+
+interface IState {
+  name: string;
+  number: string;
+}
+
+class ContactForm extends Component<IProps, IState> {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleInputChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+    this.setState({ name: value });
+  };
+
+  handleTelChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+    this.setState({ number: value });
+  };
+
+  handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    this.props.onSubmitForm(this.state);
+
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+
+  render() {
+    const { name, number } = this.state;
+    return (
+      <form className="form" onSubmit={this.handleSubmit}>
+        <label className="form_contact">
+          Name
+          <input
+            className="form_contact-input"
+            type="text"
+            name="name"
+            value={name}
+            onChange={this.handleInputChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <label className="form_contact">
+          Number
+          <input
+            className="form_contact-input"
+            type="tel"
+            name="number"
+            value={number}
+            onChange={this.handleTelChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <button className="form_contact-button" type="submit">
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Components from './01-components/Components';
-import Feedback from './02-feedback/Feedback';
-import Phonebook from './02-phonebook/Phonebook';
-import ImageFinder from './03-image-finder/ImageFinder';
+// import Components from './01-components/Components';
+// import Feedback from './02-feedback/Feedback';
+// import Phonebook from './02-phonebook/Phonebook';
+// import ImageFinder from './03-image-finder/ImageFinder';
+import Phonebook03 from './03-phonebook/Phonebook03';
 import './index.css';
 import 'modern-normalize/modern-normalize.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Components />
-    <Feedback />
-    <Phonebook />
-    <ImageFinder />
+    {/* <Components /> */}
+    {/* <Feedback /> */}
+    {/* <Phonebook /> */}
+    {/* <ImageFinder /> */}
+    <Phonebook03 />
   </React.StrictMode>,
   document.getElementById('root'),
 );

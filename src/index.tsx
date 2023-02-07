9341435a937abe6c0703197fae8212656d4d1005
AnatoliyIliev <vekok12@gmail.com>
2023-02-07 20:44:27 +0200

    refactor: change structure files 01-componets

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Components from './Components';
+import Components from './01-components/Components';
 import 'modern-normalize/modern-normalize.css';
 
 ReactDOM.render(

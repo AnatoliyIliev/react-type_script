2eb0373912813ef15bba6715657ba9f292f3881c
AnatoliyIliev <vekok12@gmail.com>
2023-02-07 20:47:45 +0200

    refactor: change structure files 01-componets

--- src/01-components/Components.tsx
+++ src/01-components/Components.tsx
@@ -1,12 +1,12 @@
-import FriendList from './01-components/friend-list/FriendList';
-import Profile from './01-components/social-profile/Profile';
-import Statistics from './01-components/statistics/Statistics';
-import TransactionHistory from './01-components/transaction-history/TransactionHistory';
+import FriendList from './friend-list/FriendList';
+import Profile from './social-profile/Profile';
+import Statistics from './statistics/Statistics';
+import TransactionHistory from './transaction-history/TransactionHistory';
 
-import friends from './01-components/friend-list/friends.json';
-import user from './01-components/social-profile/user.json';
-import data from './01-components/statistics/data.json';
-import transactions from './01-components/transaction-history/transactions.json';
+import friends from './friend-list/friends.json';
+import user from './social-profile/user.json';
+import data from './statistics/data.json';
+import transactions from './transaction-history/transactions.json';
 
 function Components() {
   return (
